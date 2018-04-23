e12dc141f41ff3eb778c9c5be048851f3a772777
weiyyed <weiyyed@163.com>
2018-04-23 23:10:18 +0800

    1

--- microblog.py
+++ microblog.py
@@ -1,5 +1,7 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-app.run(debug=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
